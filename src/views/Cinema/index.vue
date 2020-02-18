db80e795e3d27b0fd1d05f840c460b7c51b0f45a
qqwan <499907714@qq.com>
2020-02-18 13:58:25 +0800

    orgin

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main"> 
+        <Header title="七七电影"></Header>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+    name:'Movie',
+    components:{
+        Header,
+        TabBar
+    }
+}
+</script>
